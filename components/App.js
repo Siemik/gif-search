56b00188fce2155ecad4a8134ec77f45c687d104
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2019-08-26 15:25:41 +0200

    Added App component

---
+++ components/App.js
@@ -0,0 +1,18 @@
+App = React.createClass ({
+  render: function() {
+    var styles = {
+      margin: '0 auto',
+      textAlign: 'center',
+      width: '90%'
+    };
+
+    return (
+      <div style={styles}>
+        <h1>Wyszukiwarka GIFow!</h1>
+        <p>Znajdz gifa na <a href='http://giphy.com'>giphy</a>. Nacisnij enter, aby pobrać kolejne gify.</p>
+        <Search />
+        <Gif />
+      </div>
+    );
+  }
+});
