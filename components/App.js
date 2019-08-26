7c456c49cd144e37ac9a30105cf04111903207e5
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2019-08-26 17:57:10 +0200

    Added code to Gif element in App.js

--- components/App.js
+++ components/App.js
@@ -11,7 +11,18 @@ App = React.createClass ({
         <h1>Wyszukiwarka GIFow!</h1>
         <p>Znajdz gifa na <a href='http://giphy.com'>giphy</a>. Nacisnij enter, aby pobrać kolejne gify.</p>
         <Search />
-        <Gif />
+        <Gif
+          loading={this.state.loading}
+          url={this.state.gif.url}
+          sourceUrl={this.state.gif.sourceUrl}
+          getInitialState() {
+              return {
+                  loading: false,
+                  searchingText: '',
+                  gif: {}
+              };
+          },
+        />
       </div>
     );
   }
