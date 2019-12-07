14ef37efbc6f651edb04b9542c0e6e98368a2f77
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2019-12-07 18:06:46 +0100

    Changed callback to promise

--- components/App.js
+++ components/App.js
@@ -10,32 +10,40 @@ App = React.createClass ({
     this.setState({
       loading: true  // 2.
     });
-    this.getGif(searchingText, function(gif) {  // 3.
+    this.getGif(searchingText).then (gif => {  // 3.
       this.setState({  // 4
         loading: false,  // a
         gif: gif,  // b
         searchingText: searchingText  // c
       });
-    }.bind(this));
+    });
   },
-  getGif: function(searchingText, callback) {  // 1.
-    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-    var xhr = new XMLHttpRequest();  // 3.
-    xhr.open('GET', url);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-           var data = JSON.parse(xhr.responseText).data; // 4.
-            var gif = {  // 5.
+  getGif: function(searchingText) {  // 1.
+    return new Promise(
+      (resolve, reject) => {
+        const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+        let xhr = new XMLHttpRequest();  // 3.
+        xhr.open('GET', url);
+          xhr.onload = function() {
+            if (this.status === 200) {
+              let data = JSON.parse(xhr.responseText).data; // 4
+              let gif = { // 5
                 url: data.fixed_width_downsampled_url,
                 sourceUrl: data.url
-            };
-            callback(gif);  // 6.
+              };
+              resolve(gif); // 6.
+            }
+            reject(new Error(this.statusText));
+          };
+          xhr.onerror = function () {
+            reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+          };
+          xhr.send();
         }
-    };
-    xhr.send();
-  },
+      );
+    },
   render: function() {
-    var styles = {
+    let styles = {
       margin: '0 auto',
       textAlign: 'center',
       width: '90%'
